94adf6699cd02f7f71f11cb0d8440606eaa9b93f
Evgeny Petrov <evgeny@epetrov.net>
2020-08-15 06:58:40 +0200

    Initial Commit

---
+++ sane3D Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sane3D Demo
+//
+//  Created by Evgeny Petrov on 15.08.20.
+//  Copyright © 2020 Evgeny Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
